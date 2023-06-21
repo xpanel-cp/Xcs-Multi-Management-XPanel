d23eeae96979aa58717ff111a825be1eb9af9ab6
Alireza <Alirezadb1994@gmail.com>
2023-06-21 23:34:38 +0330

    XCS Multi Management of XPanel

--- Web Panel/panel/Config/database.php
+++ Web Panel/panel/Config/database.php
@@ -9,6 +9,5 @@ define("DB_HOST", "127.0.0.1");
 define("DB_NAME", $dbname);
 define("DB_USER", $username);
 define("DB_PASS", $password);
-define("PORT", $port);
 define("LANG", $lang);
 date_default_timezone_set("Asia/Tehran");
--- Web Panel/panel/Config/define.php
+++ Web Panel/panel/Config/define.php
@@ -1,6 +1,6 @@
 <?php
 $protocol = isset($_SERVER['HTTPS']) ? 'https' : 'http';
 $http_host=$_SERVER['HTTP_HOST'];
-define('path', $protocol.'://'.$http_host.'/');
+define('path', $protocol.'://'.$http_host.'/panel/');
 $fakeurl = "https://google.com";
 define('fakeurl', $fakeurl);
