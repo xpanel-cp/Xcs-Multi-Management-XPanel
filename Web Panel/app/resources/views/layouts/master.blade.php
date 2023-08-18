da1223a0a14fca32c56e4d9f44c9bf9867b1b348
Alireza <Alirezadb1994@gmail.com>
2023-08-18 09:34:30 +0330

    XCS Multi Management of XPanel v1.0

--- Web Panel/app/resources/views/layouts/master.blade.php
+++ Web Panel/app/resources/views/layouts/master.blade.php
@@ -14,20 +14,20 @@
 <!-- [ Pre-loader ] End -->
 @include('layouts.topnav')
 @php
-    $json = file_get_contents('https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/version.json');
+    $json = file_get_contents('https://raw.githubusercontent.com/xpanel-cp/Xcs-Multi-Management-XPanel/master/version.json');
     $obj = json_decode($json);
-    $github='https://github.com/xpanel-cp/XPanel-SSH-User-Management/blob/master/README-EN.md#installation-guide';
+    $github='https://github.com/xpanel-cp/Xcs-Multi-Management-XPanel/tree/master';
 @endphp
 @if($obj->last_version>37)
 
     <div class="p-4 mb-2" style="position: fixed;z-index: 9999;left: 0;">
         <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <img src="/assets/images/xlogo.png" class="img-fluid m-r-5" alt="XPanel" style="width: 17px">
+                <img src="/assets/images/xlogo.png" class="img-fluid m-r-5" alt="Xcs" style="width: 17px">
                 <strong class="me-auto">Xcs</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-            <div class="toast-body">New Version XPanel <a href="{!! $github !!}" target="_blank">Github</a> </div>
+            <div class="toast-body">New Version Xcs <a href="{!! $github !!}" target="_blank">Github</a> </div>
         </div>
     </div>
 @endif
